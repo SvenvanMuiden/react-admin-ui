b86ca5924004011de91daeaa4c7f12db8427417b
SvenvanMuiden <Svenvm2004@gmail.com>
2025-10-18 15:11:41 +0200

    Add TopBox component and update Home layout
    Introduced a new TopBox component and its SCSS file. Updated Home.tsx to include TopBox in the first grid box and adjusted class names for other boxes. Modified home.scss to define grid spans for specific boxes, improving the home page layout.

---
+++ src/components/topBox/TopBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./topbox.scss"
+
+const TopBox = () => {
+  return (
+    <div className="topBox">
+        <h1>Top Deals</h1>
+    </div>
+  )
+}
+
+export default TopBox
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,20 @@
+import TopBox from "../../components/topBox/TopBox"
 import ".//home.scss"
 
 const Home = () => {
     return (
         <div className="home">
-            <div className="box box1">Box1</div>
-            <div className="box box1">Box2</div>
-            <div className="box box1">Box3</div>
-            <div className="box box1">Box4</div>
-            <div className="box box1">Box5</div>
-            <div className="box box1">Box6</div>
-            <div className="box box1">Box7</div>
-            <div className="box box1">Box8</div>
-            <div className="box box1">Box9</div>
+            <div className="box box1">
+                <TopBox/>
+            </div>
+            <div className="box ox2">Box2</div>
+            <div className="box box3">Box3</div>
+            <div className="box box4">Box4</div>
+            <div className="box box5">Box5</div>
+            <div className="box box6">Box6</div>
+            <div className="box box7">Box7</div>
+            <div className="box box8">Box8</div>
+            <div className="box box9">Box9</div>
         </div>
     )
 }
